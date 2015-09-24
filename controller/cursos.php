a35edf9d376b34d74f76d25337f55a1f278ac7c0
geranjian2 <geranjian@gmail.com>
2015-09-24 14:53:45 -0500

    controlador

--- controller/cursos.php
+++ controller/cursos.php
@@ -1,15 +1,21 @@
 <?php 
 	$funcionCurso=$GET["op"];
 	$id_curso=$_POST["id_curso"];
-	$id_curso=$_POST["nombre_curso"];
-	$id_curso=$_POST["fecha_comienzo"];
+	$nombre_cur=$_POST["nombre_curso"];
+	$comienzo_cur=$_POST["fecha_comienzo"];
+	$error="";
 
 
 	switch ($funcionCurso) {
 		case 'insertar':
 				if(isset($_POST["enviar"]))
 				{
-
+					if($id_curso == "")
+					{
+						$error=" <script>document.getElementByID('error_id_curso').innerHTML='Que crees, el codigo es importante';
+						document.getElementById('error_id_curso').className += ' alert alert-danger';
+						</script>";
+					}
 				}
 			break;
 		
